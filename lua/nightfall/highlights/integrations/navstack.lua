f2855f2c60bcc454045d3ce54a7227335f1bec2b
Benjamin Jasper <jasper.benjamin@icloud.com>
2026-01-19 18:14:26 +0100

    feat: improve navstack design

--- lua/nightfall/highlights/integrations/navstack.lua
+++ lua/nightfall/highlights/integrations/navstack.lua
@@ -5,8 +5,8 @@ local M = {}
 ---@type nightfall.HighlightsFn
 function M.get(c)
 	local groups = {
-		NavstackCurrent = { fg = c.attention, bg = Util.darken(c.attention, 0.3) },
-		NavstackIndex = { fg = c.whiteDark, bg = Util.darken(c.whiteDark, 0.3) },
+		NavstackCurrent = { fg = c.attention, bg = Util.darken(c.attention, 0.4) },
+		NavstackIndex = { fg = c.whiteDark, bg = Util.darken(c.whiteDark, 0.4) },
 		NavstackPinned = { fg = c.darkOrange },
 	}
 
